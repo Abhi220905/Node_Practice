40ec653e19ffae7902d8b43e09ed03193cfc64ef
Abhi220905 <abhishekkhatri0022@gmail.com>
2026-01-28 23:35:28 +0530

    Database use(Atlast) and create schema & table and insert data

--- MongoDB/server.js
+++ MongoDB/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const { default: mongoose } = require('mongoose')
+const { default: mongoose, Schema, model } = require('mongoose')
 const app = express()
 
 require('dotenv').config() // Always on the top of declear
+app.use(express.urlencoded())
+app.use(express.json())
+
 
 const PORT = process.env.PORT || 5000
 
@@ -14,5 +17,62 @@ mongoose.connect(process.env.DB_URL)
 app.get("/", (_, res) => {
     res.send("Server Connect")
 })
+
+
+// create Schema and Table name
+
+// const userSchema = new mongoose.Schema({})  # another way to import schema 
+const userSchema = new Schema({
+    username: String,
+    grid: Number,
+    status: Boolean
+})
+
+
+//// table name
+
+
+const user = model('User', userSchema)
+
+
+
+//////// LOGIC  (CONTROLLER AND ROUTING(ENDPOINT))
+
+app.post('/api/users', (req, res) => {
+    // console.log(req.body);
+
+    try {
+        const { username, grid, status } = req.body;
+        user.create({ username, grid, status })
+            .then(() => res.json("Data Inserted"))
+            .catch((err) => res.json(err.message))
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+
+app.get('/api/users', async (req, res) => {
+    const records = await user.find()
+
+    // res.json({
+    //     success: true,
+    //     records : records.length > 0 ? records : "No Records"
+    // })
+
+    if (records.length > 0) {
+        res.json({
+            success: true,
+            records: records
+        })
+    } else {
+        res.json({
+            success: false,
+            records: "No Records"
+        })
+    }
+})
+
+
 
 app.listen(PORT, () => console.log(`Application running in port number http://localhost:${PORT}`))
