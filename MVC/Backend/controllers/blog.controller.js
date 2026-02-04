134ff8922e556a2728c61b84ba7146f8a460d669
Abhi220905 <abhishekkhatri0022@gmail.com>
2026-02-04 12:30:04 +0530

    Image Inserted and handle in backend

--- MVC/Backend/controllers/blog.controller.js
+++ MVC/Backend/controllers/blog.controller.js
@@ -1,24 +1,35 @@
 const Blog = require("../models/blog.model");
 
 exports.store = async (req, res) => {
-    // console.log(req.body)
-    // res.json(req.file.filename)
-    const { b_title, b_cat } = req.body;
-    Blog.create({ b_title, b_cat, b_image: req?.file?.filename })
-        // using chainning operator for the handle error , when the img is not uploading then we will show the error , 
-        // and this error can be handled by using chainning operator like this ( b_image: req?.file?.filename)
-        .then(() => res.json("Data Inserted"))
-        .catch((err) => res.json({
-            success: false,
-            message: err.message
-        }))
-}
+  // console.log(req.body)
+  // res.json(req.file.filename)
+  const { b_title, b_cat } = req.body;
+  Blog.create({ b_title, b_cat, b_image: req?.file?.filename })
+    // using chainning operator for the handle error , when the img is not uploading then we will show the error ,
+    // and this error can be handled by using chainning operator like this ( b_image: req?.file?.filename)
+    .then(() => res.json("Data Inserted"))
+    .catch((err) =>
+      res.json({
+        success: false,
+        message: err.message,
+      }),
+    );
+};
 
 exports.index = async (req, res) => {
-    const records = await Blog.find()
-    res.json({
-        success: true,
-        records: records.length > 0 ? records : "No Records"
-    })
-
-}
+  const records = await Blog.find();
+  // console.log(records)
+  const newArr = records.map((ele) => {
+    return {
+      ...ele._doc, // to get all the data from the document then we can use ...ele._doc, it's part of mongoose
+      b_image: ele.b_image
+        ? `${process.env.SERVER}/uploads/${ele.b_image}`
+        : `${process.env.DUMMY_IMG_URL}`, // if image is present then we will show the image otherwise we will show the dummy image
+    };
+  });
+  console.log(newArr);
+  res.json({
+    success: true,
+    records: newArr.length > 0 ? newArr : "No Records",
+  });
+};
--- MVC/Frontend/src/App.jsx
+++ MVC/Frontend/src/App.jsx
@@ -1,19 +1,123 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 
 const App = () => {
-
+  const [blogs, setBlogs] = useState([]);
+  const { register, handleSubmit, reset } = useForm();
   async function showApi() {
-    const res = await axios.get('http://localhost:8000/api/blogs')
-    console.log(res.data)
+    const res = await axios.get("http://localhost:8000/api/blogs");
+    setBlogs(res.data.records);
   }
 
   useEffect(() => {
-    showApi()
-  })
-  return (
-    <div>App</div>
-  )
-}
+    console.log(import.meta.env.VITE_IMAGE_URL);
+    showApi();
+  }, []);
 
-export default App
+  function AddData(data) {
+    // console.log(data.b_image[0])
+
+    // FormData is a built-in JavaScript object that allows you to easily construct a set of key/value pairs representing
+    // form fields and their values, which can then be sent using methods like XMLHttpRequest or the Fetch API.
+
+    // Append the form fields and their values to the FormData object
+    // here b_image is the name of the field in the backend
+    // and data.b_image[0] is the file selected by the user
+    // we use [0] because data.b_image is an array of files, and we want the first file
+    // if multiple files are selected then we can use a loop to append all the files
+    // here b_title and b_cat are the names of the fields in the backend
+    // and data.b_title and data.b_cat are the values entered by the user
+    // we are appending all the fields to the formData object
+    // this formData object will be sent to the backend
+    // using axios post method
+
+    const formData = new FormData();
+
+    formData.append("b_title", data.b_title);
+    formData.append("b_cat", data.b_cat);
+    formData.append("b_image", data.b_image[0]);
+
+    axios
+      .post("http://localhost:8000/api/blogs", formData)
+      .then((res) => {
+        console.log(res.data);
+        reset();
+        showApi();
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <>
+      <div className="col-lg-6 mx-auto my-5 p-5 shadow ">
+        <form action="" method="post" onSubmit={handleSubmit(AddData)}>
+          <div className="mt-4">
+            <h2 className="text-center mb-4">Add Blog</h2>
+            <select name="" id="" className="form-select">
+              <option value="" disabled selected>
+                ----Select Category----
+              </option>
+              <option value="Technology">Technology</option>
+              <option value="Health">Health</option>
+              <option value="Travel">Travel</option>
+              <option value="Food">Food</option>
+              <option value="Lifestyle">Lifestyle</option>
+            </select>
+          </div>
+
+          <div className="mt-4">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Blog Title"
+              {...register("b_title")}
+            />
+          </div>
+          <div className="mt-4">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Blog Category"
+              {...register("b_cat")}
+            />
+          </div>
+          <div className="mt-4">
+            <input
+              type="file"
+              className="form-control"
+              placeholder="Blog Image URL"
+              {...register("b_image")}
+            />
+          </div>
+          <div className="mt-4">
+            <button className="btn btn-primary w-100">Add Blog</button>
+          </div>
+        </form>
+      </div>
+
+      <div className="container my-5">
+        <h1 className="text-center mb-4">Blog Posts</h1>
+
+        <div className="row mb-4 row gap-5 row-cols-xl-5 row-cols-lg-4 row-cols-md-3 row-cols-sm-2">
+          {blogs &&
+            blogs.map((blog, index) => (
+              <div key={index} className="card mb-3 col-md-4 shadow border-0">
+                <div className="card border-0 img-top">
+                  <img src={blog?.b_image} alt={blog.b_title} />
+                </div>
+                <div className="card-body">
+                  <h5 className="card-title">{blog.b_title}</h5>
+                  <h6 className="card-title">{blog.b_cat}</h6>
+                  <p className="card-text">{blog.createdAt}</p>
+                  <p className="card-text">{blog.updatedAt}</p>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
